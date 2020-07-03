7fb935067755d3916a2ca4de2a438c941429d558
Joe Large <46566571+joejoe2009@users.noreply.github.com>
2020-07-03 15:34:00 +0100

    Media player methods added to RecordLoader.java

--- app/src/main/java/com/voicediary/RecordLoader.java
+++ app/src/main/java/com/voicediary/RecordLoader.java
@@ -1,5 +1,47 @@
 package com.voicediary;
 
 class RecordLoader {
+    
+    public void play(view v) {
+        if (player == null) {
+            player = MediaPlayer.create(context: this, R.raw.song);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+
+    public void pause(view v) {
+        if (player != null) {
+            player.pause();
+
+        }
+
+    }
+
+    public void record(view v) {}
+
+    public void stop(view v) {
+        stopPlayer();
+
+    }
+
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.maketext(context: this, text: "MediaPLayer release", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    @Override
+    protected void OnStop() {
+        super.OnStop();
+        stopPlayer();
+    }
 
 }
